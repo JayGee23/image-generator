5af49026fdc48e25a2fbafbea78fd0787b2895a0
JayGee <jayguarino@gmail.com>
2023-06-04 17:01:03 -0700

    completed frontend -basic JS, CSS, HTML

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,9 @@
 const onSubmit = (e) => {
     e.preventDefault();
 
+    document.querySelector('.msg').textContent = '';
+    document.querySelector('#image').src = '';
+
     const prompt = document.querySelector('#prompt').value;
     const size = document.querySelector('#size').value;
 
@@ -13,7 +16,46 @@ const onSubmit = (e) => {
 }
 
 const generateImageRequest = async (prompt, size) => {
-    
+    try {
+        showSpinner();
+        
+        // get response from a fetch request to this endpoint
+        const response = await fetch('openai/generateimage', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                prompt,
+                size
+            })
+        });
+
+        // check if response is okay because 'catch' wouldn't catch a 400 error
+        if(!response.ok) {
+            removeSpinner();
+            throw new Error('Sorry, but that image could not be generated');
+        }
+
+        const data = await response.json();
+        
+        // taking the data property of the data object and saving it as imageURL
+        const imageURL = data.data;
+        document.querySelector('#image').src = imageURL
+
+        removeSpinner();
+
+    } catch (error) {
+        document.querySelector('.msg').textContent = error;
+    }
+}
+
+const showSpinner = () => {
+    document.querySelector('.spinner').classList.add('show');
+}
+
+const removeSpinner = () => {
+    document.querySelector('.spinner').classList.remove('show');
 }
 
 // selects HTML element by id(image-form) and adds an eventlistener, 1st arg: event type. 2nd arg: function to use. 
